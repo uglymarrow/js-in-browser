8a3b1353bf529cf24ffc18726a1b098d0e5a096b
Ksenia <kalagaevaxenia@ya.ru>
2021-08-12 12:28:13 +0300

    Правка progressbar

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -1,27 +1,17 @@
-let xhr = new XMLHttpRequest();
-xhr.open("POST", "https://netology-slow-rest.herokuapp.com/upload.php");
 let formData = new FormData(document.forms.form);
-let send_button = document.getElementById("send");
-send_button.onclick = event => {
-    event.preventDefault();
-    xhr.send(formData);
-}
-
 let progress = document.getElementById("progress");
+let send_button = document.getElementById("send");
 
-xhr.onreadystatechange = () => {
-    switch (xhr.readyState) {
-        case 1: 
-          progress.value = 0.25;
-          break;
-        case 2: 
-          progress.value = 0.5;
-          break;
-        case 3:
-          progress.value = 0.75; 
-          break;
-        case 4:
-          progress.value = 1;
-          break;
-    }
-}
+send_button.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", "https://netology-slow-rest.herokuapp.com/upload.php");
+
+  xhr.upload.onprogress = upload_event => {
+    progress.value = (upload_event.loaded / upload_event.total);
+    alert( 'Загружено на сервер ' + upload_event.loaded + ' байт из ' + upload_event.total );
+  }
+
+  xhr.send(formData)
+})
